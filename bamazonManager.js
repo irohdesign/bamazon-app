4972d5b794c7516ceb27b28b8f2a21f779947a0c
Jalia Evans <irohdesign@outlook.com>
2019-01-14 23:00:00 -0500

    can't seem to get the query to work while nested in a function, even though it works when it's run on the global level

--- bamazonManager.js
+++ bamazonManager.js
@@ -29,21 +29,26 @@ function customerStart() {
             type: "input",
             message: "What is the ID of the product you would like to buy?",
             name: "chosenProduct"
+        },
+        {
+            type: "number",
+            message: "How many units would you like to order?",
+            name: "chosenQuantity"
         }
-    ]).then(function(response) {
-        inquirer.prompt([
-            {
-                type: "number",
-                message: "How many units would you like to order?",
-                name: "chosenQuantity"
-            }
+    
         ]).then(function(response) {
-            console.log(response.chosenQuantity);
-            checkStore(response.chosenQuantity);
-        })
-    });
-}
+            checkStock(response.chosenProduct, response.chosenQuantity);
+        });
+    }
 
-function checkStore(x) {
-    console.log(x);
+
+
+ function checkStock(chosenProduct, chosenQuantity) {   
+    var query = connection.query("SELECT stock_quantity FROM products WHERE product_name = 'fish slippers'", function(err, res) {
+        console.log(res);
+    
+        console.log(chosenProduct + chosenQuantity);
+    });
+
+    console.log(query.sql);
 }
