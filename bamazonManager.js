145bfeb8b32ee744373625f22c674b2656448e92
Jalia Evans <irohdesign@outlook.com>
2019-01-14 22:05:02 -0500

    got inquirer to pass quantity to an outside function so it'll check things without making a huge chunk of unreadable code

--- bamazonManager.js
+++ bamazonManager.js
@@ -12,10 +12,38 @@ var connection = mysql.createConnection( {
 
 connection.connect(function(err) {
     console.log("Connected!");
+    customerStart();
     connection.end();
 })
 
 // showing objects for sale
-connection.query("SELECT * FROM products", function(err, res){
-    console.log(res);
-});
+function showAll() {
+    connection.query("SELECT * FROM products", function(err, res){
+        console.log(res);
+    })
+}
+
+function customerStart() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the ID of the product you would like to buy?",
+            name: "chosenProduct"
+        }
+    ]).then(function(response) {
+        inquirer.prompt([
+            {
+                type: "number",
+                message: "How many units would you like to order?",
+                name: "chosenQuantity"
+            }
+        ]).then(function(response) {
+            console.log(response.chosenQuantity);
+            checkStore(response.chosenQuantity);
+        })
+    });
+}
+
+function checkStore(x) {
+    console.log(x);
+}
