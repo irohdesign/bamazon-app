e5b43c04c392e4ef3b0f860ab2a39d47832d756d
Jalia Evans <irohdesign@outlook.com>
2019-01-14 21:50:40 -0500

    connected to database, and remember to end the connection

--- bamazonManager.js
+++ bamazonManager.js
@@ -2,4 +2,15 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+var connection = mysql.createConnection( {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
 
+connection.connect(function(err) {
+    console.log("Connected!");
+    connection.end();
+})
